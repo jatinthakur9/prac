1f0bba4eb4c3913c940657a2b4b2ee4e820fb4e6
jatin_thakur9 <tjatin29@gmail.com>
2023-07-13 11:17:11 +0530

    setstaes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:prac/july/11thjuly/tappbar.dart';
 
 import 'package:prac/july/7thjuly/whatsappfrontscreen.dart';
+import 'package:prac/july/13thjuly/state.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false, home: Whatsappfront());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: states());
   }
 }
